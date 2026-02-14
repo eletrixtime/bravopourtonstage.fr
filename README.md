8419fa32025816edad0f259abc9e233e48a7de75
Plucky <hello@plucky.lol>
2026-02-14 12:43:16 +0000

    feat: hello world

---
+++ README.md
@@ -0,0 +1,13 @@
+# bravopourtonstage.fr
+
+Le but du site ? Tout est dans le titre !
+
+10€ le nom de domaine pour une blague de merde, but why not :)
+
+## Droits d'auteur
+
+Ce site a un but humoristique.
+
+Source : [Mister V](https://youtu.be/h5BORTfWjPo?t=303)
+
+Réclamation : [legal+bpts@plucky.lol](mailto:legal+bpts@plucky.lol)
---
+++ src/js/index.ts
@@ -0,0 +1,23 @@
+const text = document.getElementById("text")!;
+const button = document.getElementById("button")!;
+const video = document.getElementById("video") as HTMLVideoElement;
+
+button.addEventListener(
+  "click",
+  () => {
+    button.hidden = true;
+    video.hidden = false;
+    video.play();
+    video.requestFullscreen();
+
+    video.addEventListener(
+      "ended",
+      () => {
+        video.hidden = true;
+        text.hidden = false;
+      },
+      { once: true },
+    );
+  },
+  { once: true },
+);
